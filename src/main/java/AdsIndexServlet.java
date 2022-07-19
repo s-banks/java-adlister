85ba6505d15b72bd65a7fa36dda874cdb7baf7a0
Shane Banks <shane.banks02@gmail.com>
2022-07-19 12:03:24 -0500

    sessions exercise complete

--- src/main/java/AdsIndexServlet.java
+++ src/main/java/AdsIndexServlet.java
@@ -3,13 +3,24 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
 
 @WebServlet(name = "AdsIndexServlet", urlPatterns = "/ads")
 public class AdsIndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        Ads adsDao = DaoFactory.getAdsDao();
+        List<Ad> ads = adsDao.all();
+        String username;
+        if (session.getAttribute("username") == null) {
+            username = " there";
+        } else {
+            username = ", " + session.getAttribute("username");
+        }
         request.setAttribute("ads", DaoFactory.getAdsDao().all());
+        request.setAttribute("username", username);
         request.getRequestDispatcher("/ads/index.jsp").forward(request, response);
     }
 }
--- src/main/webapp/ads/index.jsp
+++ src/main/webapp/ads/index.jsp
@@ -10,8 +10,8 @@
 <jsp:include page="/partials/navbar.jsp" />
 
 <div class="container">
-    <h1>Here Are all the ads!</h1>
-
+<%--    <h1>Here Are all the ads!</h1>--%>
+    <h1> Hey${username}, here are all of the ads.</h1>
     <c:forEach var="ad" items="${ads}">
         <div class="col-md-6">
             <h2>${ad.title}</h2>
